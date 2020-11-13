99333b5b093c7a5ae4b05da657909b49f06aee71
Malte Blomqvist <blomqvist.malte@telia.com>
2020-11-13 01:35:59 +0100

    Emulator & Example works

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,99 +1,47 @@
-#![allow(unused)]
+use crate::language_specs::*;
+use std::{fs, io::Write};
 
-use std::iter::Peekable;
-
-const INSTRUCT_LEN: u8 = 8;
-const ADDR_LEN: u8 = 5;
-
-#[repr(u8)]
-enum OPCODE {
-    ADD = 0b000,
-    SUB = 0b001,
-    IFEQ = 0b010,
-    ADDI = 0b011,
-    SUBI = 0b100,
-    LI = 0b101,
-    J = 0b110,
-    SYSCALL = 0b111,
-}
-#[repr(u8)]
-enum REGISTRY {
-    R1 = 0b00,
-    R2 = 0b01,
-    R3 = 0b10,
-    R4 = 0b11,
-}
-
-#[repr(u8)]
-enum SYSCALL {
-    PRINT = 0b00,
-    READ = 0b01,
-    TERMINATE = 0b10,
-}
-
-const OP_DICTIONARY: [(char, u8); 8] = [
-    ('💘', OPCODE::ADD as u8),
-    ('💔', OPCODE::SUB as u8),
-    ('🤔', OPCODE::IFEQ as u8),
-    ('👆', OPCODE::ADDI as u8),
-    ('👇', OPCODE::SUBI as u8),
-    ('👈', OPCODE::LI as u8),
-    ('♿', OPCODE::J as u8),
-    ('🤖', OPCODE::SYSCALL as u8),
-];
-
-const REG_DICTIONARY: [(char, u8); 4] = [
-    ('🍩', REGISTRY::R1 as u8),
-    ('👀', REGISTRY::R2 as u8),
-    ('🍎', REGISTRY::R3 as u8),
-    ('🍊', REGISTRY::R4 as u8),
-];
-
-const SYSCALL_DICTIONARY: [(char, u8); 3] = [
-    ('📢', SYSCALL::PRINT as u8),
-    ('📜', SYSCALL::READ as u8),
-    ('🔪', SYSCALL::TERMINATE as u8),
-];
-
-const MACRO: char = '🔓';
-const END_MACRO: char = '🔒';
-const CODE: char = '💬';
-
-const SECTION: [char; 3] = [MACRO, END_MACRO, CODE];
-
-pub fn compile(code: String) -> Result<(), String> {
-    let (macros, code_start) = parse_macros(&code)?;
+pub fn compile(code: String, output_name: &str) -> Result<(), String> {
+    let macros = parse_macros(&code)?;
     let code = insert_macros(code, macros)?;
-    let mut code_lines = code.lines().map(|l| {
+    let code_lines = code.lines().map(|l| {
         l.split_whitespace()
             .take_while(|w| !w.starts_with('#'))
             .map(|w| w.to_string())
     });
-
     let mut executable: Vec<u8> = Vec::new();
     let mut labels: Vec<(String, u32)> = Vec::new();
     let mut pc = 0;
-    for mut line in code_lines {
-        pc += 1;
-        if let Some(bits) = parse_line(line, &mut labels, pc)
+    for line in code_lines {
+        if let Some(bits) = parse_line(line, &mut labels, &mut pc)
             .map_err(|err| err + format!(" at line {}", pc).as_str())?
         {
-            println!("{:#010b}", bits);
             executable.push(bits);
         }
     }
-    Ok(())
+    make_executable(executable, &output_name)
 }
 
-fn parse_macros(code: &String) -> Result<(Vec<(String, String)>, u32), String> {
+fn make_executable(binary: Vec<u8>, output_name: &str) -> Result<(), String> {
+    if let Ok(mut file) = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(output_name.to_string() + ".formexe")
+    {
+        return file
+            .write_all(binary.as_slice())
+            .map_err(|err| err.to_string());
+    }
+    Err("Error making executable".to_string())
+}
+
+fn parse_macros(code: &str) -> Result<Vec<(String, String)>, String> {
     let code = code.lines();
     let mut in_macro = false;
     let mut macro_name = String::new();
     let mut macr: String = String::new();
     let mut macros: Vec<(String, String)> = Vec::new();
-    let mut pc: u32 = 0;
-    for mut line in code {
-        pc += 1;
+    for line in code {
         if in_macro {
             if line.contains(END_MACRO) {
                 macros.push((macro_name.clone(), macr));
@@ -113,7 +61,7 @@ fn parse_macros(code: &String) -> Result<(Vec<(String, String)>, u32), String> {
                 .to_string();
             in_macro = true;
         } else if line.contains(CODE) {
-            return Ok((macros, pc));
+            return Ok(macros);
         }
     }
     Err("Error parsing macros".to_string())
@@ -146,13 +94,14 @@ fn insert_macros(code: String, macros: Vec<(String, String)>) -> Result<String, 
 fn parse_line<I: Iterator<Item = String>>(
     mut line: I,
     labels: &mut Vec<(String, u32)>,
-    pc: u32,
+    pc: &mut u32,
 ) -> Result<Option<u8>, String> {
     let mut instruction: u8 = 0;
     if let Some(word) = line.next() {
         if word.ends_with(':') {
             let word = word.replace(":", "");
-            labels.push((word, pc));
+            labels.push((word, *pc));
+
             return Ok(None);
         } else {
             let op_code = parse_op(word)?;
@@ -162,13 +111,13 @@ fn parse_line<I: Iterator<Item = String>>(
             } else if op_code <= 160 {
                 instruction += parse_i(line)?;
             } else if op_code == 192 {
-                instruction += parse_jump(line, labels, pc)?;
+                instruction += parse_jump(line, labels, *pc)?;
             } else {
                 instruction += parse_call(line)?;
             }
+            *pc += 1;
             return Ok(Some(instruction));
         }
-        return Err("Error parsing line".to_string());
     }
     Ok(None)
 }
@@ -215,6 +164,9 @@ fn parse_i<I: Iterator<Item = String>>(mut line: I) -> Result<u8, String> {
     if let (Some(reg1), Some(imm)) = (line.next(), line.next()) {
         if let (Ok(mut reg1), Ok(imm)) = (parse_reg(reg1), imm.parse::<u8>()) {
             reg1 <<= 3;
+            if imm > 7 {
+                return Err("Error in i-type message, too large a value!".to_string());
+            }
             let bits = reg1 + imm;
             return Ok(bits);
         }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::{env, fs};
+mod compiler;
 use compiler::compile;
-use std::{env, fs, io};
+mod emulator;
+use emulator::execute;
+mod language_specs;
 fn main() {
     let mut args = env::args();
     args.next();
-    if let Some(filepath) = args.next() {
+    if let (Some(filepath), Some(output_file)) = (args.next(), args.next()) {
         if let Ok(code) = fs::read_to_string(filepath) {
-            compile(code).unwrap(); //DONT UNWRAP! HANDLE
+            compile(code, &output_file).unwrap(); //DONT UNWRAP! HANDLE
+            execute(output_file + ".formexe").unwrap();
         } else {
             println!("Error reading code file...")
         }
