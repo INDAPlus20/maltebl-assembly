89911b29ed022425fb771e284aab67d20bf488c3
Malte Blomqvist <blomqvist.malte@telia.com>
2020-11-13 13:25:45 +0100

    Fixed md files

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,20 @@ mod language_specs;
 fn main() {
     let mut args = env::args();
     args.next();
-    if let (Some(filepath), Some(output_file)) = (args.next(), args.next()) {
-        if let Ok(code) = fs::read_to_string(filepath) {
-            compile(code, &output_file).unwrap(); //DONT UNWRAP! HANDLE
-            execute(output_file + ".formexe").unwrap();
-        } else {
-            println!("Error reading code file...")
+    if let (Some(arg), Some(filepath)) = (args.next(), args.next()) {
+        if arg == "compile" {
+            if let (Ok(code), Some(output_file)) =
+                (fs::read_to_string(filepath + ".forme"), args.next())
+            {
+                compile(code, &output_file).unwrap();
+            } else {
+                println!("Error reading code file and or output file path...");
+                panic!();
+            }
+        } else if arg == "run" {
+            execute(filepath + ".formexe").unwrap();
         }
     } else {
-        println!("Error finding file...")
+        println!("Error parsing arguments...")
     }
 }
